7bd649fe6ec90b5a37905ed64ec24dbd397fa4f9
Alex Bondarenko <bond.alexx@gmail.com>
2023-02-22 02:11:56 +0200

    finished 2 chapter

---
+++ Migrations/20230221235423_AddApplicationToDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rocky_project.Migrations
+{
+    public partial class AddApplicationToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplictionType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplictionType", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplictionType");
+        }
+    }
+}
